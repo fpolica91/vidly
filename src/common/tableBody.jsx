142dc1550dbefbd32fa8b034f087bb29f4dd5d7b
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2019-06-10 16:53:49 -0400

    using rendercell

--- src/common/tableBody.jsx
+++ src/common/tableBody.jsx
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import _ from "lodash";
 
 class TableBody extends Component {
+  // remember that column.content is a function reference
+  // here we check to see if column.content exists, if it does return the function
+  //passing item, which is the movie
+
+  renderCell = (item, column) => {
+    if (column.content) return column.content(item);
+
+    return _.get(item, column);
+  };
+
   render() {
     const { data, columns } = this.props;
     return (
@@ -9,7 +19,7 @@ class TableBody extends Component {
         {data.map(item => (
           <tr>
             {columns.map(column => (
-              <td>{_.get(item, column.path)}</td>
+              <td>{this.renderCell(item, column)}</td>
             ))}
           </tr>
         ))}
